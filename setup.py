85c0f75c5bf6ac218e20080b4f036f519b8fd74b
psychowico <psychowico@ubuntu>
2012-07-20 17:54:40 +0200

    preparing sql alchemy scaffolding

--- setup.py
+++ setup.py
@@ -1,18 +1,48 @@
-from setuptools import setup
+import os
 
-setup(
-    name='projectksi',
-    version='0.01',
-    packages=[''],
-    url='',
-    license='',
-    author='psychowico',
-    author_email='',
-    description='',
-    install_requires = [
-        'pyramid >= 1.3.2'
-    ],
-    dependency_links = [
-        'https://github.com/psychowico/pyramid_viewscomposer/tarball/master'
-    ],
-)
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='projectksi',
+      version='0.01',
+      description='projectksi',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='projectksi',
+      install_requires=requires,
+      dependency_links = [
+          'https://github.com/psychowico/pyramid_viewscomposer/tarball/master'
+      ],
+      entry_points="""\
+      [paste.app_factory]
+      main = projectksi:main
+      [console_scripts]
+      initialize_projectksi_db = projectksi.scripts.initializedb:main
+      """,
+      )
+
