7fc52b66dbc0cc9f3b9c048a34eb2ec2029e658d
psychowico <psychowico@ubuntu>
2012-07-20 14:41:11 +0200

    adding default setup.py and empty, main python package

---
+++ sources/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='projectksi',
+    version='0.01',
+    packages=[''],
+    url='',
+    license='',
+    author='psychowico',
+    author_email='',
+    description='',
+    install_requires = [
+        'pyramid >= 1.3.2'
+    ],
+    dependency_links = [
+        'https://github.com/psychowico/pyramid_viewscomposer/tarball/master'
+    ],
+)
