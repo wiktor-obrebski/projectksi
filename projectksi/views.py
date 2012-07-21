fb0215755133ae145f356033d44f922f2d9ebb5e
psychowico <psychowico@ubuntu>
2012-07-21 18:56:46 +0200

    override default sample template by our jinja2 template, default boostrap sample, adding jquery support and our main css file

--- projectksi/views.py
+++ projectksi/views.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='test_template.jinja2')
 def my_view(request):
     return {'project':'projectksi'}
