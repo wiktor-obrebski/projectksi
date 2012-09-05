39127661e929aa4b8b3c377c4b0cafca1a697fa0
psychowico <psychowico@ubuntu>
2012-09-05 08:23:54 +0200

    removing old, not used, webdeps code

--- projectksi/core/jinja2_extend.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import contextfunction
-
-def WebDepsIncluder(type):
-    @contextfunction
-    def include(context, files, **extra_context):
-        method = getattr(context['request'].deps, type)
-        method(files)
-        return ''
-    return include
